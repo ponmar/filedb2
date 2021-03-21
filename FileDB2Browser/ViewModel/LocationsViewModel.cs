5440f9667b81241fd18616ddb0a96ce748d66e1f
Pontus Markström <pontus.markstrom@gmail.com>
2021-03-21 12:43:09 +0100

    Keep track of selected location and tag

--- FileDB2Browser/ViewModel/LocationsViewModel.cs
+++ FileDB2Browser/ViewModel/LocationsViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using FileDB2Interface;
 using FileDB2Interface.Model;
 
@@ -20,12 +21,28 @@ namespace FileDB2Browser.ViewModel
 
     public class LocationsViewModel
     {
+        public ICommand LocationSelectionCommand
+        {
+            get
+            {
+                return locationSelectionCommand ??= new CommandHandler(LocationSelectionChanged);
+            }
+        }
+        private ICommand locationSelectionCommand;
+
         public ObservableCollection<Location> Locations { get; }
+
+        private Location selectedLocation;
 
         public LocationsViewModel(FileDB2Handle fileDB2Handle)
         {
             var locations = fileDB2Handle.GetLocations().Select(lm => new Location() { Name = lm.name, Description = lm.description, Position = lm.position });
             Locations = new ObservableCollection<Location>(locations);
+        }
+
+        public void LocationSelectionChanged(object parameter)
+        {
+            selectedLocation = (Location)parameter;
         }
     }
 }
--- FileDB2Browser/ViewModel/TagsViewModel.cs
+++ FileDB2Browser/ViewModel/TagsViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using FileDB2Interface;
 using FileDB2Interface.Model;
 
@@ -16,12 +17,28 @@ namespace FileDB2Browser.ViewModel
 
     public class TagsViewModel
     {
+        public ICommand TagSelectionCommand
+        {
+            get
+            {
+                return tagSelectionCommand ??= new CommandHandler(TagSelectionChanged);
+            }
+        }
+        private ICommand tagSelectionCommand;
+
         public ObservableCollection<Tag> Tags { get; }
+
+        private Tag selectedTag;
 
         public TagsViewModel(FileDB2Handle fileDB2Handle)
         {
             var tags = fileDB2Handle.GetTags().Select(tm => new Tag() { Name = tm.name });
             Tags = new ObservableCollection<Tag>(tags);
+        }
+
+        public void TagSelectionChanged(object parameter)
+        {
+            selectedTag = (Tag)parameter;
         }
     }
 }
