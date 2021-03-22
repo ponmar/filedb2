4b3bfa7e476a4cb2fc0b1478b6ede8e92bb00de1
Pontus Markström <pontus.markstrom@gmail.com>
2021-03-22 23:17:55 +0100

    Initial work on the import page

--- FileDB2Browser/ViewModel/ImportViewModel.cs
+++ FileDB2Browser/ViewModel/ImportViewModel.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using FileDB2Interface;
 
 namespace FileDB2Browser.ViewModel
 {
+    public class NewFile
+    {
+        public string Path { get; set; }
+    }
+
     public class ImportViewModel
     {
+        public ICommand ScanNewFilesCommand
+        {
+            get
+            {
+                return scanNewFilesCommand ??= new CommandHandler(ScanNewFiles);
+            }
+        }
+        private ICommand scanNewFilesCommand;
+
+        private readonly FileDB2Handle fileDB2Handle;
+
+        public List<NewFile> NewFiles;
+
         public ImportViewModel(FileDB2Handle fileDB2Handle)
         {
+            this.fileDB2Handle = fileDB2Handle;
+        }
+
+        public void ScanNewFiles(object parameter)
+        {
+            NewFiles = fileDB2Handle.ListNewFilesystemFiles().Select(p => new NewFile() { Path = p }).ToList();
         }
     }
 }
