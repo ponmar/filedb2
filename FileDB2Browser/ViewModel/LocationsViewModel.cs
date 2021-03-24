38297c56f2b7a9c569c967976d6558ad2d58d1a8
Pontus Markström <pontus.markstrom@gmail.com>
2021-03-24 16:03:48 +0100

    Commands for removing and editing tags and locations

--- FileDB2Browser/ViewModel/LocationsViewModel.cs
+++ FileDB2Browser/ViewModel/LocationsViewModel.cs
@@ -17,10 +17,40 @@ namespace FileDB2Browser.ViewModel
         public string Description { get; set; }
 
         public string Position { get; set; }
+
+        private readonly int id;
+
+        public Location(int id)
+        {
+            this.id = id;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
     }
 
     public class LocationsViewModel
     {
+        public ICommand RemoveLocationCommand
+        {
+            get
+            {
+                return removeLocationCommand ??= new CommandHandler(RemoveLocation);
+            }
+        }
+        private ICommand removeLocationCommand;
+
+        public ICommand EditLocationCommand
+        {
+            get
+            {
+                return editLocationCommand ??= new CommandHandler(EditLocation);
+            }
+        }
+        private ICommand editLocationCommand;
+
         public ICommand LocationSelectionCommand
         {
             get
@@ -34,15 +64,46 @@ namespace FileDB2Browser.ViewModel
 
         private Location selectedLocation;
 
+        private readonly FileDB2Handle fileDB2Handle;
+
         public LocationsViewModel(FileDB2Handle fileDB2Handle)
         {
-            var locations = fileDB2Handle.GetLocations().Select(lm => new Location() { Name = lm.name, Description = lm.description, Position = lm.position });
+            this.fileDB2Handle = fileDB2Handle;
+            var locations = GetLocations();
             Locations = new ObservableCollection<Location>(locations);
+        }
+
+        public void RemoveLocation(object parameter)
+        {
+            if (selectedLocation != null)
+            {
+                // TODO: only delete if location not used in files?
+                fileDB2Handle.DeleteLocation(selectedLocation.GetId());
+
+                Locations.Clear();
+                foreach (var location in GetLocations())
+                {
+                    Locations.Add(location);
+                }
+            }
+        }
+
+        public void EditLocation(object parameter)
+        {
+            if (selectedLocation != null)
+            {
+                // TODO: edit in new window
+            }
         }
 
         public void LocationSelectionChanged(object parameter)
         {
             selectedLocation = (Location)parameter;
+        }
+
+        private IEnumerable<Location> GetLocations()
+        {
+            return fileDB2Handle.GetLocations().Select(lm => new Location(lm.id) { Name = lm.name, Description = lm.description, Position = lm.position });
         }
     }
 }
--- FileDB2Browser/ViewModel/TagsViewModel.cs
+++ FileDB2Browser/ViewModel/TagsViewModel.cs
@@ -13,10 +13,40 @@ namespace FileDB2Browser.ViewModel
     public class Tag
     {
         public string Name { get; set; }
+
+        private readonly int id;
+
+        public Tag(int id)
+        {
+            this.id = id;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
     }
 
     public class TagsViewModel
     {
+        public ICommand RemoveTagCommand
+        {
+            get
+            {
+                return removeTagCommand ??= new CommandHandler(RemoveTag);
+            }
+        }
+        private ICommand removeTagCommand;
+
+        public ICommand EditTagCommand
+        {
+            get
+            {
+                return editTagCommand ??= new CommandHandler(EditTag);
+            }
+        }
+        private ICommand editTagCommand;
+
         public ICommand TagSelectionCommand
         {
             get
@@ -30,15 +60,46 @@ namespace FileDB2Browser.ViewModel
 
         private Tag selectedTag;
 
+        private readonly FileDB2Handle fileDB2Handle;
+
         public TagsViewModel(FileDB2Handle fileDB2Handle)
         {
-            var tags = fileDB2Handle.GetTags().Select(tm => new Tag() { Name = tm.name });
+            this.fileDB2Handle = fileDB2Handle;
+            var tags = GetTags();
             Tags = new ObservableCollection<Tag>(tags);
+        }
+
+        public void RemoveTag(object parameter)
+        {
+            if (selectedTag != null)
+            {
+                // TODO: only delete if tag not used in files?
+                fileDB2Handle.DeleteTag(selectedTag.GetId());
+
+                Tags.Clear();
+                foreach (var tag in GetTags())
+                {
+                    Tags.Add(tag);
+                }
+            }
+        }
+
+        public void EditTag(object parameter)
+        {
+            if (selectedTag != null)
+            {
+                // TODO: edit in new window
+            }
         }
 
         public void TagSelectionChanged(object parameter)
         {
             selectedTag = (Tag)parameter;
+        }
+
+        private IEnumerable<Tag> GetTags()
+        {
+            return fileDB2Handle.GetTags().Select(tm => new Tag(tm.id) { Name = tm.name });
         }
     }
 }
