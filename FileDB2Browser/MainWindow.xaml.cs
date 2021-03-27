601253aa1e8c7b419bbbcdf168abeee0a9da61bc
Pontus Markström <pontus.markstrom@gmail.com>
2021-03-27 19:43:44 +0100

    Main view uses bindings

--- FileDB2Browser/MainWindow.xaml.cs
+++ FileDB2Browser/MainWindow.xaml.cs
@@ -18,118 +18,21 @@ using FileDB2Interface;
 
 namespace FileDB2Browser
 {
-    enum Page { Start, Find, Birthdays, Persons, Locations, Tags, Import, Tools }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Page currentPage;
-
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
 
             if (!FileDB2BrowserConfigIO.FileExists())
             {
                 if (!FileDB2BrowserConfigIO.Write(new FileDB2BrowserConfig()))
                 {
                     // TODO: show error dialog
-                }
-            }
-
-            SetPage(Page.Start, true);
-        }
-
-        private void SetStartPage(object sender, RoutedEventArgs e)
-        {
-            SetPage(Page.Start);
-        }
-
-        private void SetFindPage(object sender, RoutedEventArgs e)
-        {
-            SetPage(Page.Find);
-        }
-
-        private void SetBirthdaysPage(object sender, RoutedEventArgs e)
-        {
-            SetPage(Page.Birthdays);
-        }
-
-        private void SetPersonsPage(object sender, RoutedEventArgs e)
-        {
-            SetPage(Page.Persons);
-        }
-
-        private void SetLocationsPage(object sender, RoutedEventArgs e)
-        {
-            SetPage(Page.Locations);
-        }
-
-        private void SetTagsPage(object sender, RoutedEventArgs e)
-        {
-            SetPage(Page.Tags);
-        }
-
-        private void SetImportPage(object sender, RoutedEventArgs e)
-        {
-            SetPage(Page.Import);
-        }
-
-        private void SetToolsPage(object sender, RoutedEventArgs e)
-        {
-            SetPage(Page.Tools);
-        }
-
-        private void SetPage(Page page, bool force = false)
-        {
-            if (page != currentPage || force)
-            {
-                currentPage = page;
-                StartPage.Visibility = Visibility.Collapsed;
-                FindPage.Visibility = Visibility.Collapsed;
-                BirthdaysPage.Visibility = Visibility.Collapsed;
-                PersonsPage.Visibility = Visibility.Collapsed;
-                LocationsPage.Visibility = Visibility.Collapsed;
-                TagsPage.Visibility = Visibility.Collapsed;
-                ImportPage.Visibility = Visibility.Collapsed;
-                ToolsPage.Visibility = Visibility.Collapsed;
-
-                switch (page)
-                {
-                    case Page.Start:
-                        //DataContext = new StartViewModel(fileDB2Handle);
-                        StartPage.Visibility = Visibility.Visible;
-                        break;
-                    case Page.Find:
-                        //DataContext = new FindViewModel(fileDB2Handle, this);
-                        FindPage.Visibility = Visibility.Visible;
-                        break;
-                    case Page.Birthdays:
-                        //DataContext = new BirthdaysViewModel(fileDB2Handle);
-                        BirthdaysPage.Visibility = Visibility.Visible;
-                        break;
-                    case Page.Persons:
-                        //DataContext = new PersonsViewModel(fileDB2Handle);
-                        PersonsPage.Visibility = Visibility.Visible;
-                        break;
-                    case Page.Locations:
-                        //DataContext = new LocationsViewModel(fileDB2Handle);
-                        LocationsPage.Visibility = Visibility.Visible;
-                        break;
-                    case Page.Tags:
-                        //DataContext = new TagsViewModel(fileDB2Handle);
-                        TagsPage.Visibility = Visibility.Visible;
-                        break;
-                    case Page.Import:
-                        //DataContext = new ImportViewModel(fileDB2Handle, browserConfig);
-                        ImportPage.Visibility = Visibility.Visible;
-                        break;
-                    case Page.Tools:
-                        //DataContext = new ToolsViewModel(fileDB2Handle);
-                        ToolsPage.Visibility = Visibility.Visible;
-                        break;
                 }
             }
         }
---
+++ FileDB2Browser/ViewModel/MainViewModel.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FileDB2Browser.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand OpenStartPageCommand
+        {
+            get
+            {
+                return openStartPageCommand ??= new CommandHandler(OpenStartPage);
+            }
+        }
+        private ICommand openStartPageCommand;
+
+        public ICommand OpenFindPageCommand
+        {
+            get
+            {
+                return openFindPageCommand ??= new CommandHandler(OpenFindPage);
+            }
+        }
+        private ICommand openFindPageCommand;
+
+        public ICommand OpenBirthdaysPageCommand
+        {
+            get
+            {
+                return openBirthdaysPageCommand ??= new CommandHandler(OpenBirthdaysPage);
+            }
+        }
+        private ICommand openBirthdaysPageCommand;
+
+        public ICommand OpenPersonsPageCommand
+        {
+            get
+            {
+                return openPersonsPageCommand ??= new CommandHandler(OpenPersonsPage);
+            }
+        }
+        private ICommand openPersonsPageCommand;
+
+        public ICommand OpenLocationsPageCommand
+        {
+            get
+            {
+                return openLocationsPageCommand ??= new CommandHandler(OpenLocationsPage);
+            }
+        }
+        private ICommand openLocationsPageCommand;
+
+        public ICommand OpenTagsPageCommand
+        {
+            get
+            {
+                return openTagsPageCommand ??= new CommandHandler(OpenTagsPage);
+            }
+        }
+        private ICommand openTagsPageCommand;
+
+        public ICommand OpenImportPageCommand
+        {
+            get
+            {
+                return openImportPageCommand ??= new CommandHandler(OpenImportPage);
+            }
+        }
+        private ICommand openImportPageCommand;
+
+        public ICommand OpenToolsPageCommand
+        {
+            get
+            {
+                return openToolsPageCommand ??= new CommandHandler(OpenToolsPage);
+            }
+        }
+        private ICommand openToolsPageCommand;
+
+
+        public bool StartPageActive
+        {
+            get => startPageActive;
+            private set
+            {
+                if (value != startPageActive)
+                {
+                    startPageActive = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(StartPageActive)));
+                }
+            }
+        }
+        private bool startPageActive = true;
+
+        public bool FindPageActive
+        {
+            get => findPageActive;
+            private set
+            {
+                if (value != findPageActive)
+                {
+                    findPageActive = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(FindPageActive)));
+                }
+            }
+        }
+        private bool findPageActive = false;
+
+        public bool BirthdaysPageActive
+        {
+            get => birthdaysPageActive;
+            private set
+            {
+                if (value != birthdaysPageActive)
+                {
+                    birthdaysPageActive = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(BirthdaysPageActive)));
+                }
+            }
+        }
+        private bool birthdaysPageActive = false;
+
+        public bool PersonsPageActive
+        {
+            get => personsPageActive;
+            private set
+            {
+                if (value != personsPageActive)
+                {
+                    personsPageActive = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(PersonsPageActive)));
+                }
+            }
+        }
+        private bool personsPageActive = false;
+
+        public bool LocationsPageActive
+        {
+            get => locationsPageActive;
+            private set
+            {
+                if (value != locationsPageActive)
+                {
+                    locationsPageActive = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(LocationsPageActive)));
+                }
+            }
+        }
+        private bool locationsPageActive = false;
+
+        public bool TagsPageActive
+        {
+            get => tagsPageActive;
+            private set
+            {
+                if (value != tagsPageActive)
+                {
+                    tagsPageActive = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(TagsPageActive)));
+                }
+            }
+        }
+        private bool tagsPageActive = false;
+
+        public bool ImportPageActive
+        {
+            get => importPageActive;
+            private set
+            {
+                if (value != importPageActive)
+                {
+                    importPageActive = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(ImportPageActive)));
+                }
+            }
+        }
+        private bool importPageActive = false;
+
+        public bool ToolsPageActive
+        {
+            get => toolsPageActive;
+            private set
+            {
+                if (value != toolsPageActive)
+                {
+                    toolsPageActive = value;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(ToolsPageActive)));
+                }
+            }
+        }
+        private bool toolsPageActive = false;
+
+        public MainViewModel()
+        {
+        }
+
+        public void OpenStartPage(object parameter)
+        {
+            ResetActivePage();
+            StartPageActive = true;
+        }
+
+        public void OpenFindPage(object parameter)
+        {
+            ResetActivePage();
+            FindPageActive = true;
+        }
+
+        public void OpenBirthdaysPage(object parameter)
+        {
+            ResetActivePage();
+            BirthdaysPageActive = true;
+        }
+
+        public void OpenPersonsPage(object parameter)
+        {
+            ResetActivePage();
+            PersonsPageActive = true;
+        }
+
+        public void OpenLocationsPage(object parameter)
+        {
+            ResetActivePage();
+            LocationsPageActive = true;
+        }
+
+        public void OpenTagsPage(object parameter)
+        {
+            ResetActivePage();
+            TagsPageActive = true;
+        }
+
+        public void OpenImportPage(object parameter)
+        {
+            ResetActivePage();
+            ImportPageActive = true;
+        }
+
+        public void OpenToolsPage(object parameter)
+        {
+            ResetActivePage();
+            ToolsPageActive = true;
+        }
+
+        private void ResetActivePage()
+        {
+            StartPageActive = false;
+            FindPageActive = false;
+            BirthdaysPageActive = false;
+            PersonsPageActive = false;
+            LocationsPageActive = false;
+            TagsPageActive = false;
+            ImportPageActive = false;
+            ToolsPageActive = false;
+        }
+    }
+}
