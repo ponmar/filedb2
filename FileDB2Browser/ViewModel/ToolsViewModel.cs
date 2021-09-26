c8765a4f549bd4beb2f6e64b3a01b71a47e0adf1
Pontus Markström <pontus.markstrom@gmail.com>
2021-09-26 15:56:58 +0200

    Tools page can make database backup

--- FileDB2Browser/ViewModel/ToolsViewModel.cs
+++ FileDB2Browser/ViewModel/ToolsViewModel.cs
@@ -1,16 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using FileDB2Interface;
 
 namespace FileDB2Browser.ViewModel
 {
+    public class BackupFile
+    {
+        public string Filename { get; }
+
+        public BackupFile(string filename)
+        {
+            Filename = filename;
+        }
+    }
+
     public class ToolsViewModel : ViewModelBase
     {
+        public ICommand CreateBackupCommand => createBackupCommand ??= new CommandHandler(CreateBackup);
+        private ICommand createBackupCommand;
+
+        public ObservableCollection<BackupFile> BackupFiles { get; } = new();
+
+        private readonly FileDB2Handle fileDB2Handle;
+
         public ToolsViewModel(FileDB2Handle fileDB2Handle)
         {
+            this.fileDB2Handle = fileDB2Handle;
+            ScanBackupFiles();
+        }
+
+        public void CreateBackup(object parameter)
+        {
+            var db = Utils.BrowserConfig.Database;
+            if (File.Exists(db))
+            {
+                var directoryPath = Path.GetDirectoryName(db);
+                var timestamp = DateTime.Now.ToString("yyyy-MM-dd_HHmmss");
+                var backupFilename = $"backup_{timestamp}.db";
+                var backupFilePath = Path.Combine(directoryPath, backupFilename);
+                if (!File.Exists(backupFilePath))
+                {
+                    File.Copy(db, backupFilePath);
+                    ScanBackupFiles();
+                }
+            }
+        }
+
+        private void ScanBackupFiles()
+        {
+            var backupDir = Path.GetDirectoryName(Utils.BrowserConfig.Database);
+            if (Directory.Exists(backupDir))
+            {
+                BackupFiles.Clear();
+                foreach (var backupFile in Directory.GetFiles(backupDir, "backup_*.db"))
+                {
+                    BackupFiles.Add(new BackupFile(backupFile));
+                }
+            }
+
         }
     }
 }
