18896ccfb9bf715503de1fc898ab1b1645a2f591
Pontus Markström <pontus.markstrom@combitech.com>
2021-11-11 15:57:43 +0100

    Rename FileDB2Browser project to FileDB2

--- release.bat
+++ release.bat
@@ -1,11 +1,9 @@
 rem @echo off
 
-set appDir=FileDB2Browser\bin\Release\net5.0-windows
-set versionPrinter=
-
 for /f "delims=" %%a in ('VersionPrinter\bin\Release\net5.0\VersionPrinter.exe') do @set version=%%a
 echo Detected version: %version%
 
+set appDir=FileDB2\bin\Release\net5.0-windows
 set zipDir=FileDB2-%version%
 set releaseDir=release\%zipDir%
 set zipFilename=%zipDir%.zip
