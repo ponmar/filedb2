9ac17562f6799cb6454c17ca5c729e053bcf25c9
Pontus Markström <pontus.markstrom@combitech.com>
2021-11-12 15:43:16 +0100

    Add about page

---
+++ FileDB/ViewModel/AboutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace FileDB.ViewModel
+{
+    public class AboutViewModel : ViewModelBase
+    {
+        public string DownloadLink => "https://drive.google.com/drive/folders/1GyZpdDcMdUOlvvtwtKUuylazoy7XaIcm?usp=sharing";
+
+        // TODO: update path when changes included in release
+        public string Changes => File.ReadAllText(@"C:\repos\filedb2\docs\CHANGES.txt");
+
+        public AboutViewModel()
+        {
+        }
+    }
+}
