32443440bb0c092f929f141b2dd7f59c53c73b33
Pontus Markström <pontus.markstrom@gmail.com>
2021-11-13 14:19:08 +0100

    Add empty demo database

--- FileDBInterface/DatabaseUtils.cs
+++ FileDBInterface/DatabaseUtils.cs
@@ -15,20 +15,9 @@ namespace FileDBInterface
                 throw new DatabaseWrapperException($"Database already created: {database}");
             }
 
-            var scriptPath = "filedb.sql";
-            string sql = null;
-            try
-            {
-                sql = File.ReadAllText(scriptPath);
-            }
-            catch (IOException e)
-            {
-                throw new DatabaseWrapperException("Unable to load database creation script", e);
-            }
-
             SQLiteConnection.CreateFile(database);
             using var connection = CreateConnection(database);
-            connection.Query(sql);
+            connection.Query(DatabaseCreationSql);
         }
 
         internal static IDbConnection CreateConnection(string database)
@@ -36,5 +25,56 @@ namespace FileDBInterface
             var connectionString = $"Data Source={database};foreign keys = true";
             return new SQLiteConnection(connectionString);
         }
+
+        private const string DatabaseCreationSql = @"
+create table files(
+    id integer primary key autoincrement not null,
+    path text unique not null, /* Format: path/to/file/filename */
+    description text,
+    datetime varchar(19), /* Format: YYYY, YYYY-MM-DD or YYYY-MM-DDTHH:MM:SS */
+    position text /* Format: <latitude> <longitude> */
+);
+
+create table persons(
+    id integer primary key autoincrement not null,
+    firstname text not null,
+    lastname text not null,
+    description text,
+    dateofbirth varchar(10), /* Format: YYYY-MM-DD */
+    deceased varchar(10), /* Format: YYYY-MM-DD */
+    profilefileid integer references files(id) on delete set null,
+    sex integer not null default 0 /* Values according to ISO/IEC 5218 (0=Not known, 1=Male, 2=Female, 9=Not applicable) */
+);
+
+create table locations(
+    id integer primary key autoincrement not null,
+    name text unique not null,
+    description text,
+    position text /* Format: <latitude> <longitude> */
+);
+
+create table tags(
+    id integer primary key autoincrement not null,
+    name text unique not null
+);
+
+create table filepersons(
+    fileid integer references files(id) on delete cascade,
+    personid integer references persons(id) on delete cascade,
+    primary key(fileid, personid)
+);
+
+create table filelocations(
+    fileid integer references files(id) on delete cascade,
+    locationid integer references locations(id) on delete cascade,
+    primary key(fileid, locationid)
+);
+
+create table filetags(
+    fileid integer references files(id) on delete cascade,
+    tagid integer references tags(id) on delete cascade,
+    primary key(fileid, tagid)
+);
+";
     }
 }
