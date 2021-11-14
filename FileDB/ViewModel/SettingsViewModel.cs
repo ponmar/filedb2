7245d2fb251cbee6ed3fab1c081b5bb7ed0a93b9
Pontus Markström <pontus.markstrom@gmail.com>
2021-11-14 11:39:04 +0100

    Move backup handling to tools page

--- FileDB/ViewModel/SettingsViewModel.cs
+++ FileDB/ViewModel/SettingsViewModel.cs
@@ -12,25 +12,8 @@ using Newtonsoft.Json;
 
 namespace FileDB.ViewModel
 {
-    public class BackupFile
+     public class SettingsViewModel : ViewModelBase
     {
-        public string Filename { get; }
-
-        public DateTime Timestamp { get; }
-
-        public TimeSpan Age => DateTime.Now - Timestamp;
-
-        public BackupFile(string filename, DateTime timestamp)
-        {
-            Filename = filename;
-            Timestamp = timestamp;
-        }
-    }
-
-    public class SettingsViewModel : ViewModelBase
-    {
-        private const string BackupFileTimestampFormat = "yyyy-MM-ddTHHmmss";
-
         public string ConfigName
         {
             get => configName;
@@ -116,9 +99,6 @@ namespace FileDB.ViewModel
         public ICommand CreateDatabaseCommand => createDatabaseCommand ??= new CommandHandler(CreateDatabase, CreateDatabasePossible);
         private ICommand createDatabaseCommand;
 
-        public ICommand CreateBackupCommand => createBackupCommand ??= new CommandHandler(CreateBackup);
-        private ICommand createBackupCommand;
-
         public ICommand SetDefaultSlideshowDelayCommand => setDefaultSlideshowDelayCommand ??= new CommandHandler(x => SlideshowDelay = ((int)BrowserConfigFactory.CreateDefaultConfig().SlideshowDelay.TotalSeconds).ToString());
         private ICommand setDefaultSlideshowDelayCommand;
 
@@ -140,13 +120,9 @@ namespace FileDB.ViewModel
         public ICommand SetDefaultReadOnlyCommand => setDefaultReadOnlyCommand ??= new CommandHandler(x => ReadOnly = BrowserConfigFactory.CreateDefaultConfig().ReadOnly);
         private ICommand setDefaultReadOnlyCommand;
 
-        public ObservableCollection<BackupFile> BackupFiles { get; } = new();
-
         public SettingsViewModel()
         {
             UpdateFromConfiguration();
-            ScanBackupFiles();
-            ShowBackupReminder();
         }
 
         private void UpdateFromConfiguration()
@@ -326,77 +302,6 @@ namespace FileDB.ViewModel
         public bool CreateDatabasePossible()
         {
             return !File.Exists(Database);
-        }
-
-        public void CreateBackup()
-        {
-            var db = Utils.BrowserConfig.Database;
-            if (File.Exists(db))
-            {
-                var directoryPath = Path.GetDirectoryName(db);
-                var timestamp = DateTime.Now.ToString(BackupFileTimestampFormat);
-                var backupFilename = $"backup_{timestamp}.db";
-                var backupFilePath = Path.Combine(directoryPath, backupFilename);
-                if (!File.Exists(backupFilePath))
-                {
-                    File.Copy(db, backupFilePath);
-                    ScanBackupFiles();
-                }
-                else
-                {
-                    Utils.ShowErrorDialog($"Backup file already available");
-                }
-            }
-            else
-            {
-                Utils.ShowErrorDialog($"Missing database: {db}");
-            }
-        }
-
-        private void ScanBackupFiles()
-        {
-            var backupDir = Path.GetDirectoryName(Utils.BrowserConfig.Database);
-            if (Directory.Exists(backupDir))
-            {
-                BackupFiles.Clear();
-                foreach (var filePath in Directory.GetFiles(backupDir, "backup_*.db"))
-                {
-                    var filenameParts = filePath.Split("_");
-                    if (filenameParts.Length >= 2)
-                    {
-                        var timestampString = filenameParts[filenameParts.Length - 1].Replace(".db", "");
-                        try
-                        {
-                            var timestamp = DateTime.ParseExact(timestampString, BackupFileTimestampFormat, null);
-                            BackupFiles.Add(new BackupFile(filePath, timestamp));
-                        }
-                        catch (FormatException)
-                        {
-                        }
-                    }
-                }
-            }
-        }
-
-        private void ShowBackupReminder()
-        {
-            if (Utils.BrowserConfig.StartupBackupReminderAfterDays <= 0)
-            {
-                return;
-            }
-
-            if (BackupFiles.Count == 0)
-            {
-                Utils.ShowWarningDialog("Backup reminder: No database backup has been created!");
-            }
-            else
-            {
-                var latestBackupDaysAge = (int)BackupFiles.Min(x => x.Age).TotalDays;
-                if (latestBackupDaysAge >= Utils.BrowserConfig.StartupBackupReminderAfterDays)
-                {
-                    Utils.ShowWarningDialog($"Backup reminder: Last database backup created {latestBackupDaysAge} days ago!");
-                }
-            }
         }
     }
 }
