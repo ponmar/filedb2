5f3e79ad9bdeeaa34b804935a20622d936024088
Pontus Markström <pontus.markstrom@combitech.com>
2021-11-26 07:28:37 +0100

    Add unittests for FileDB project

---
+++ FileDBTests/Validators/ConfigValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿using FileDB.Config;
+using FileDB.Validators;
+using FluentValidation.TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileDBTests.Validators
+{
+    [TestClass]
+    public class ConfigValidatorTests
+    {
+        private ConfigValidator validator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            validator = new();
+        }
+
+        [TestMethod]
+        public void Validate_DefaultConfig_Valid()
+        {
+            var config = DefaultConfigs.Default;
+            var result = validator.TestValidate(config);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [TestMethod]
+        public void Validate_DemoConfig_Valid()
+        {
+            var config = DefaultConfigs.CreateDemo();
+            var result = validator.TestValidate(config);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
