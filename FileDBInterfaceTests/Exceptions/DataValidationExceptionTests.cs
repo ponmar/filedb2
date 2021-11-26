42d276871a9e0255bafdd5c5b67b7245e2d55a71
Pontus Markström <pontus.markstrom@combitech.com>
2021-11-26 15:41:19 +0100

    Add unittest

---
+++ FileDBInterfaceTests/Exceptions/DataValidationExceptionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileDBInterface.Exceptions;
+using FluentValidation.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileDBInterfaceTests.Exceptions
+{
+    [TestClass]
+    public class DataValidationExceptionTests
+    {
+        [TestMethod]
+        public void Message()
+        {
+            var failures = new List<ValidationFailure>()
+            {
+                new ValidationFailure("Property1", "Error1"),
+                new ValidationFailure("Property2", "Error2"),
+            };
+            var result = new ValidationResult(failures);
+
+            var e = new DataValidationException(result);
+            Assert.AreEqual("Error1\nError2", e.Message);
+        }
+    }
+}
