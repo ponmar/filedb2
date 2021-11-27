bc9506805f371ed621f765acc26c45983ed1a433
Pontus Markström <pontus.markstrom@gmail.com>
2021-11-27 10:22:31 +0100

    Hide some controls when the binding value is an empty collection

--- FileDB/Converters/EnumerationToVisibilityConverter.cs
+++ FileDB/Converters/EnumerationToVisibilityConverter.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Windows;
 using System.Windows.Data;
 
 namespace FileDB.Converters
 {
-    public class StringNullOrEmptyToVisibilityConverter : System.Windows.Markup.MarkupExtension, IValueConverter
+    public class EnumerationToVisibilityConverter : System.Windows.Markup.MarkupExtension, IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return string.IsNullOrEmpty(value as string) ? Visibility.Collapsed : Visibility.Visible;
+            return value != null && ((IEnumerable)value).GetEnumerator().MoveNext() ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
---
+++ FileDBTests/Converters/EnumerationToVisibilityConverterTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using FileDB.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileDBTests.Converters
+{
+    [TestClass]
+    public class EnumerationToVisibilityConverterTests
+    {
+        [TestMethod]
+        public void Convert_Null_Collapsed()
+        {
+            var converter = new EnumerationToVisibilityConverter();
+            List<string> items = null;
+            Assert.AreEqual(Visibility.Collapsed, converter.Convert(items, typeof(IEnumerable), null, CultureInfo.InvariantCulture));
+        }
+
+        [TestMethod]
+        public void Convert_Empty_Collapsed()
+        {
+            var converter = new EnumerationToVisibilityConverter();
+            var items = new List<string>();
+            Assert.AreEqual(Visibility.Collapsed, converter.Convert(items, typeof(IEnumerable), null, CultureInfo.InvariantCulture));
+        }
+
+        [TestMethod]
+        public void Convert_HasContent_Visible()
+        {
+            var converter = new EnumerationToVisibilityConverter();
+            var items = new List<string>() { "item" };
+            Assert.AreEqual(Visibility.Visible, converter.Convert(items, typeof(IEnumerable), null, CultureInfo.InvariantCulture));
+        }
+    }
+}
