7ef95fad82c77e8a07bbc961a2fce2bdaf1fdbd5
Pontus Markström <pontus.markstrom@gmail.com>
2021-12-07 16:16:14 +0100

    Load demo configuration is no local configuration file exists

--- FileDB/App.xaml.cs
+++ FileDB/App.xaml.cs
@@ -20,13 +20,20 @@ namespace FileDB
 
             bool demoModeEnabled = e.Args.Any(x => x == "--demo");
 
+            var appDataConfig = new AppDataConfig<Config.Config>(Utils.ApplicationName);
+
             if (demoModeEnabled)
             {
                 Utils.Config = DefaultConfigs.CreateDemo();
             }
+            else if (!appDataConfig.FileExists())
+            {
+                Utils.ShowInfoDialog($"No local {Utils.ApplicationName} configuration file exists. Loading demo configuration.");
+                Utils.Config = DefaultConfigs.CreateDemo();
+            }
             else
             {
-                Utils.Config = new AppDataConfig<Config.Config>(Utils.ApplicationName).Read() ?? DefaultConfigs.Default;
+                Utils.Config = appDataConfig.Read() ?? DefaultConfigs.Default;
 
                 var validator = new ConfigValidator();
                 var result = validator.Validate(Utils.Config);
