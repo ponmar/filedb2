65f3822e079b012c4e40f52452922b3acaedf932
Pontus Markström <pontus.markstrom@gmail.com>
2022-01-08 19:20:23 +0100

    Export meta-data in json and xml format

---
+++ FileDB/Export/ExportedData.cs
@@ -0,0 +1,28 @@
+﻿using FileDBInterface.Model;
+using System;
+using System.Collections.Generic;
+
+namespace FileDB.Export
+{
+    public class ExportedFile
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public string Datetime { get; set; }
+        public string Position { get; set; }
+        public List<int> PersonIds { get; set; }
+        public List<int> LocationIds { get; set; }
+        public List<int> TagIds { get; set; }
+    }
+
+    public class ExportedData
+    {
+        public string About { get; set; }
+        public DateTime CreationDate { get; set; }
+        public List<ExportedFile> Files { get; set; }
+        public List<PersonModel> Persons { get; set; }
+        public List<LocationModel> Locations { get; set; }
+        public List<TagModel> Tags { get; set; }
+    }
+}
