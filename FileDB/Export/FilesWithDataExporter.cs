4c16c9e58538e2885f2e594763ac6c5c60cffcba
Pontus Markström <pontus.markstrom@combitech.com>
2022-01-11 14:36:33 +0100

    Draw bg rect for exported images with data

--- FileDB/Export/FilesWithDataExporter.cs
+++ FileDB/Export/FilesWithDataExporter.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Collections.Generic;
 
 namespace FileDB.Export
 {
@@ -24,10 +25,39 @@ namespace FileDB.Export
                 var bitmap = new Bitmap(sourceFilePath);
                 using (Graphics graphics = Graphics.FromImage(bitmap))
                 {
-                    using var font = new Font("Arial", 10);
-                    graphics.DrawString("testing", font, Brushes.Blue, new PointF(10f, 10f));
-                    //graphics.MeasureString();
-                    //graphics.DrawRectangle();
+                    using var font = new Font("Arial", 18);
+
+                    var textLines = new List<string>();
+                    textLines.Add("testing");
+
+                    var rectPadding = 20;
+                    var rectMargin = 20;
+
+                    var bgRect = new Rectangle(rectMargin, rectMargin, 2*rectPadding, 2*rectPadding);
+
+                    int lineHeight = 0;
+                    foreach (var textLine in textLines)
+                    {
+                        var size = graphics.MeasureString(textLine, font);
+                        if (size.Width + 2*rectPadding > bgRect.Width)
+                        {
+                            bgRect.Width = (int)size.Width + 2*rectPadding;
+                        }
+                        bgRect.Height += (int)size.Height;
+                        lineHeight = (int)size.Height;
+                    }
+
+                    var bgColor = Color.FromArgb(229, 37, 37, 38);
+                    var textBgBrush = new SolidBrush(bgColor);
+                    
+                    graphics.FillRectangle(textBgBrush, bgRect);
+
+                    var pos = new PointF(bgRect.X + rectPadding, bgRect.Y + rectPadding);
+                    foreach (var textLine in textLines)
+                    {
+                        graphics.DrawString(textLine, font, Brushes.White, pos);
+                        pos.Y += lineHeight;
+                    }
                 }
 
                 var destFilePath = Path.Combine(subdir, $"{index}.png");
