e13a7078fce4928ef9cb4336f24c75c75bbb7ab7
Pontus Markström <pontus.markstrom@gmail.com>
2022-05-27 15:26:34 +0200

    Fixes for added view

--- FileDBApp/View/RipPage.xaml.cs
+++ FileDBApp/View/RipPage.xaml.cs
@@ -1,9 +1,12 @@
+using FileDBApp.ViewModel;
+
 namespace FileDBApp.View;
 
 public partial class RipPage : ContentPage
 {
-	public RipPage()
+	public RipPage(RipViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
 }
