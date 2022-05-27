28d05121c0009478dddaccfe3dea9abc595a0a22
Pontus Markström <pontus.markstrom@combitech.com>
2022-05-27 11:46:36 +0200

    Cleanup

--- FileDBApp/Services/PersonService.cs
+++ FileDBApp/Services/PersonService.cs
@@ -13,12 +13,6 @@ namespace FileDBApp.Services
 
             var data = JsonConvert.DeserializeObject<ExportedDatabaseFileFormat>(contents);
             return data.Persons;
-
-            /*
-            var persons = data.Persons.Where(x => x.DateOfBirth != null && x.Deceased == null).Select(x => new Person(x)).ToList();
-            persons.Sort(new PersonsByDaysLeftUntilBirthdaySorter());
-            persons.ForEach(x => Persons.Add(x));
-            */
         }
     }
 }
