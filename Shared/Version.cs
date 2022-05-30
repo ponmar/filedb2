3491e50770ea8293cc801caa9270404b45d9280f
Pontus Markström <pontus.markstrom@gmail.com>
2022-05-30 20:53:12 +0200

    Prepare for release

--- Shared/Version.cs
+++ Shared/Version.cs
@@ -2,7 +2,7 @@
 {
     public class ReleaseInformation
     {
-        public static Version Version = new(2, 11);
+        public static Version Version = new(2, 12);
 
         public const string ApplicationDownloadUrl = "https://drive.google.com/drive/folders/1GyZpdDcMdUOlvvtwtKUuylazoy7XaIcm";
     }
