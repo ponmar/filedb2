6550d882985f25179279f4e4596000106aae8d2d
Pontus Markström <pontus.markstrom@gmail.com>
2022-11-09 17:28:44 +0100

    Upgrade projects to .net 7

--- release.bat
+++ release.bat
@@ -15,7 +15,7 @@ if not %ERRORLEVEL%==0 (
     echo "Build failed" && exit /b 1
 )
 
-set appDir=FileDB\bin\Release\net6.0-windows
+set appDir=FileDB\bin\Release\net7.0-windows
 set zipDir=FileDB-%version%
 set releaseDir=release\%zipDir%
 set zipFilename=%zipDir%.zip
