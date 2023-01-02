8dcef907a25760798af18b5d67a1394c0c7c660f
Pontus Markström <pontus.markstrom@gmail.com>
2023-01-02 22:58:47 +0100

    Add project for downloading files from media devices and grouping them by date

---
+++ MediaFilesHelper/MediaFileGrouper.cs
@@ -0,0 +1,88 @@
+﻿using FileDBInterface.FilesystemAccess;
+using FileDBShared.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MediaFilesHelper;
+
+public class FileInfo
+{
+    public string FilePath { get; }
+    public DateTime? DateTime { get; }
+    public DateOnly? Date => DateTime != null ? DateOnly.FromDateTime(DateTime.Value) : null;
+    public FileMetadata Metadata { get; }
+
+    public string? DestinationPath { get; set; }
+
+    public FileInfo(string filePath, FileMetadata metadata)
+    {
+        FilePath = filePath;
+        Metadata = metadata;
+        DateTime = DatabaseParsing.ParseFilesDatetime(metadata.Datetime);
+    }
+}
+
+public class MediaFileGrouper
+{
+    private readonly string directory;
+
+    public MediaFileGrouper(string directory)
+    {
+        this.directory = directory;
+    }
+
+    public List<FileInfo> ListFiles()
+    {
+        var filenames = Directory.GetFiles(directory, "*.*");
+        var result = new List<FileInfo>();
+
+        var filesystemAccess = new FilesystemAccess() { FilesRootDirectory = "not used" };
+
+        foreach (var filename in filenames)
+        {
+            var fileMetadata = filesystemAccess.ParseFileMetadata(filename);
+            if (fileMetadata != null)
+            {
+                result.Add(new FileInfo(filename, fileMetadata));
+            }
+        }
+
+        return result;
+    }
+
+    public void GroupImagesByDate(List<FileInfo> files)
+    {
+        foreach (var file in files.Where(x => x.DateTime != null))
+        {
+            var groupPath = GetFileGroupName(file);
+            var numImagesInGroup = Directory.Exists(groupPath) ? Directory.GetFiles(groupPath).Count() : 0;
+            numImagesInGroup += files.Count(x => x.Date == file.Date);
+
+            if (numImagesInGroup > 1)
+            {
+                file.DestinationPath = Path.Combine(groupPath, Path.GetFileName(file.FilePath));
+            }
+        }
+    }
+
+    public void MoveFiles(List<FileInfo> files)
+    {
+        foreach (var file in files.Where(x => x.DestinationPath != null))
+        {
+            if (!Directory.Exists(file.DestinationPath))
+            {
+                var groupDirPath = Path.GetDirectoryName(file.DestinationPath)!;
+                Directory.CreateDirectory(groupDirPath);
+            }
+            File.Move(file.FilePath, file.DestinationPath!);
+        }
+    }
+
+    private string GetFileGroupName(FileInfo fileInfo)
+    {
+        string subDir = fileInfo.DateTime!.Value.ToString("yyyy-MM-dd") + " - todo";
+        return Path.Combine(Path.GetDirectoryName(fileInfo.FilePath)!, subDir);
+    }
+}
---
+++ MediaFilesHelper/ViewModel/MainViewModel.cs
@@ -0,0 +1,147 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MediaDevices;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace MediaFilesHelper.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    private const string DefaultDownloadResult = "No files downloaded";
+
+    [ObservableProperty]
+    private string title = "MediaFilesHelper";
+
+    public Dialogs Dialogs { get; set; } = new();
+
+    [ObservableProperty]
+    private ObservableCollection<string> devices = new();
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(DeviceIsSelected))]
+    private string? selectedDevice;
+
+    partial void OnSelectedDeviceChanged(string? value)
+    {
+        DownloadResult = DefaultDownloadResult;
+    }
+
+    public bool DeviceIsSelected => selectedDevice != null;
+
+    [ObservableProperty]
+    private string? tempDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "MediaFilesHelper");
+
+    [ObservableProperty]
+    private string downloadResult = DefaultDownloadResult;
+
+    public MainViewModel()
+    {
+        RefreshDevices();
+    }
+
+    [RelayCommand]
+    private void RefreshDevices()
+    {
+        SelectedDevice = null;
+        DownloadResult = DefaultDownloadResult;
+
+        devices.Clear();
+        foreach (var device in MediaDevice.GetDevices())
+        {
+            devices.Add(device.FriendlyName);
+        }
+    }
+
+    [RelayCommand]
+    private void BrowseDestinationDirectory()
+    {
+        var selectedDir = Dialogs.BrowseExistingDirectory(tempDir, "Browse destination directory");
+        if (selectedDir != null)
+        {
+            TempDir = selectedDir;
+        }
+    }
+
+    [RelayCommand]
+    private void DownloadFilesFromDevice()
+    {
+        if (selectedDevice == null)
+        {
+            return;
+        }
+        if (string.IsNullOrEmpty(tempDir))
+        {
+            return;
+        }
+
+        if (!Directory.Exists(tempDir))
+        {
+            Directory.CreateDirectory(tempDir);
+        }
+
+        using var device = MediaDevice.GetDevices().First(d => d.FriendlyName == selectedDevice || d.Description == selectedDevice);
+        device.Connect();
+
+        // TODO: need to check "Internal shared storage" to support all devices?
+        var devicePhotoDir = device.GetDirectoryInfo(@"\Internal storage\DCIM\Camera");
+        var files = devicePhotoDir.EnumerateFiles("*.*", SearchOption.TopDirectoryOnly);
+
+        if (!Dialogs.ShowConfirmDialog(Title, $"Download {files.Count()} from selected device?"))
+        {
+            return;
+        }
+
+        var numDownloadedFiles = 0;
+        var numSkippedFiles = 0;
+
+        foreach (var file in files)
+        {
+            var destinationFilePath = Path.Combine(tempDir, file.Name);
+            if (File.Exists(destinationFilePath))
+            {
+                numSkippedFiles++;
+                continue;
+            }
+
+            using var fs = new FileStream(destinationFilePath, FileMode.Create, FileAccess.Write);
+            device.DownloadFile(file.FullName, fs);
+            numDownloadedFiles++;
+        }
+        device.Disconnect();
+
+        DownloadResult = $"{numDownloadedFiles} files downloaded ({numSkippedFiles} skipped)";
+    }
+
+    [RelayCommand]
+    private void ShowDownloadedMediaFiles()
+    {
+        if (!string.IsNullOrEmpty(TempDir))
+        {
+            OpenDirectoryInExplorer(TempDir);
+        }
+    }
+
+    private static void OpenDirectoryInExplorer(string path)
+    {
+        Process.Start("explorer.exe", "/open, " + path);
+    }
+
+    [RelayCommand]
+    private void GroupImages()
+    {
+        if (!Directory.Exists(TempDir))
+        {
+            Dialogs.ShowErrorDialog(Title, $"Missing directory: {TempDir}");
+            return;
+        }
+
+        var mediaFileGrouper = new MediaFileGrouper(TempDir);
+        var files = mediaFileGrouper.ListFiles();
+        mediaFileGrouper.GroupImagesByDate(files);
+        mediaFileGrouper.MoveFiles(files);
+    }
+}
