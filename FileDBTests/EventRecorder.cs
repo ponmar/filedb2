bddceb8f9985962abfc1adf24dac728bb9aa6b66
Pontus Markström <pontus.markstrom@gmail.com>
2023-03-08 09:34:03 +0100

    Add event handling classes

---
+++ FileDBTests/EventRecorder.cs
@@ -0,0 +1,56 @@
+﻿using FileDB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FileDBTests;
+
+public class EventRecorder
+{
+    private readonly List<object> events = new();
+
+    public void Record<T>() where T : class
+    {
+        this.RegisterForEvent<T>(events.Add);
+    }
+
+    public void Reset()
+    {
+        events.Clear();
+    }
+
+    public IEnumerable<T> GetRecording<T>() where T : class
+    {
+        return events.Where(x => x is T).Cast<T>();
+    }
+
+    public void AssertEventRecorded<T>(int? numEvents = null)
+    {
+        if (numEvents == null)
+        {
+            Assert.IsTrue(events.Any(x => x is T));
+        }
+        else
+        {
+            Assert.AreEqual(numEvents, events.Count(x => x is T));
+        }
+    }
+
+    public void AssertNoEventsRecorded()
+    {
+        Assert.IsTrue(!events.Any());
+    }
+}
+
+public class SingleEventRecorder<T> : EventRecorder where T : class
+{
+    public SingleEventRecorder()
+    {
+        Record<T>();
+    }
+
+    public void AssertEventRecorded(int? numEvents = null)
+    {
+        AssertEventRecorded<T>(numEvents);
+    }
+}
---
+++ FileDBTests/EventsTests.cs
@@ -0,0 +1,46 @@
+﻿using FileDB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace FileDBTests;
+
+record Event();
+
+[TestClass]
+public class EventsTests
+{
+    private readonly EventRecorder recorder = new();
+
+    [TestInitialize]
+    public void Init()
+    {
+        recorder.Reset();
+        recorder.Record<Event>();
+    }
+
+    [TestMethod]
+    public void Send()
+    {
+        var event1 = new Event();
+        Events.Send(event1);
+        recorder.AssertEventRecorded<Event>(1);
+
+        var event2 = new Event();
+        Events.Send(event2);
+        recorder.AssertEventRecorded<Event>(2);
+
+        var recording = recorder.GetRecording<Event>().ToList();
+        Assert.AreSame(event1, recording[0]);
+        Assert.AreSame(event2, recording[1]);
+    }
+
+    [TestMethod]
+    public void Send_TypeArg()
+    {
+        Events.Send<Event>();
+        recorder.AssertEventRecorded<Event>(1);
+
+        Events.Send<Event>();
+        recorder.AssertEventRecorded<Event>(2);
+    }
+}
