feb546376a676c794703e382d0acbf440dba7d48
Pontus Markström <pontus.markstrom@gmail.com>
2024-03-24 19:38:19 +0100

    Cleanup

--- FileDBAvalonia/ViewModels/Dialogs/BrowseDirectoriesViewModel.cs
+++ FileDBAvalonia/ViewModels/Dialogs/BrowseDirectoriesViewModel.cs
@@ -24,8 +24,6 @@ namespace FileDBAvalonia.ViewModels.Dialogs
 
     public partial class BrowseSubDirectoriesViewModel : ObservableObject
     {
-        private const string RootDirectoryName = "root";
-
         [ObservableProperty]
         private List<Directory> directories = [];
 
@@ -44,32 +42,9 @@ namespace FileDBAvalonia.ViewModels.Dialogs
 
         private void ReloadDirectoryTree()
         {
-            var root = new Directory(RootDirectoryName);
-
             Directories.Clear();
-            Directories.Add(root);
-
-            foreach (var directoryPath in dbAccessProvider.DbAccess.GetDirectories())
-            {
-                var pathParts = directoryPath.Split("/");
-
-                if (pathParts.Length > 0)
-                {
-                    var currentDirectory = root;
-
-                    foreach (var pathPart in pathParts)
-                    {
-                        var subDirectory = currentDirectory.Directories.FirstOrDefault(x => x.Name == pathPart);
-                        if (subDirectory is null)
-                        {
-                            subDirectory = new Directory(pathPart, currentDirectory);
-                            currentDirectory.Directories.Add(subDirectory);
-                        }
-
-                        currentDirectory = subDirectory;
-                    }
-                }
-            }
+            var dirs = DirectoryTreeBuilder.Build(dbAccessProvider.DbAccess.GetDirectories());
+            dirs.ForEach(Directories.Add);
         }
 
         [RelayCommand]
@@ -78,18 +53,54 @@ namespace FileDBAvalonia.ViewModels.Dialogs
             if (SelectedDirectory is not null)
             {
                 var path = SelectedDirectory.Path;
-                if (path.StartsWith(RootDirectoryName))
-                {
-                    path = path.Substring(RootDirectoryName.Length);
-                }
-                if (path.StartsWith("/"))
-                {
-                    path = path.Substring("/".Length);
-                }
                 SelectedDirectoryPath = path;
-
                 Messenger.Send<CloseModalDialogRequest>();
             }
+        }
+    }
+
+    public class DirectoryTreeBuilder
+    {
+        public static List<Directory> Build(IEnumerable<string> directoryPaths)
+        {
+            var result = new List<Directory>();
+
+            foreach (var directoryPath in directoryPaths)
+            {
+                var pathParts = directoryPath.Split("/");
+
+                if (pathParts.Length > 0)
+                {
+                    Directory? currentDirectory = null;
+
+                    bool isTopLevel = true;
+                    foreach (var pathPart in pathParts)
+                    {
+                        var subDirectory = currentDirectory?.Directories.FirstOrDefault(x => x.Name == pathPart);
+                        if (subDirectory is null)
+                        {
+                            subDirectory = new Directory(pathPart, currentDirectory);
+                            if (isTopLevel)
+                            {
+                                result.Add(subDirectory);
+                            }
+                            if (currentDirectory is null)
+                            {
+                                currentDirectory = subDirectory;
+                            }
+                            else
+                            {
+                                currentDirectory.Directories.Add(subDirectory!);
+                            }
+                        }
+
+                        currentDirectory = subDirectory;
+                        isTopLevel = false;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
