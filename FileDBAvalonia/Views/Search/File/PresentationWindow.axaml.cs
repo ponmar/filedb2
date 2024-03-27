b49f8b98d887e30ed9894b6dc8b0168aca8a743d
Pontus Markström <pontus.markstrom@gmail.com>
2024-03-27 21:34:42 +0100

    Fix doubleclick on presentation window

--- FileDBAvalonia/Views/Search/File/PresentationWindow.axaml.cs
+++ FileDBAvalonia/Views/Search/File/PresentationWindow.axaml.cs
@@ -10,5 +10,19 @@ namespace FileDBAvalonia.Views.Search.File
             InitializeComponent();
             DataContext = ServiceLocator.Resolve<FileViewModel>();
         }
+
+        private void Window_DoubleTapped(object? sender, Avalonia.Input.TappedEventArgs e)
+        {
+            if (WindowState == WindowState.FullScreen)
+            {
+                WindowState = WindowState.Normal;
+                SystemDecorations = SystemDecorations.Full;
+            }
+            else
+            {
+                WindowState = WindowState.FullScreen;
+                SystemDecorations = SystemDecorations.None;
+            }
+        }
     }
 }
