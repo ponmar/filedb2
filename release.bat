9b64e94b555d6ab1d967c80b223396710ad46a49
Pontus Markström <pontus.markstrom@gmail.com>
2024-03-29 21:26:20 +0100

    Release script includes the Avalonia application

--- release.bat
+++ release.bat
@@ -10,13 +10,13 @@ if not %ERRORLEVEL%==0 (
     echo "Clean solution failed" && exit /b 1
 )
 
-dotnet publish FileDB\FileDB.csproj -r win-x64 -p:Version=%version%.0.0 --self-contained true
+dotnet publish FileDBAvalonia.Desktop\FileDBAvalonia.Desktop.csproj -r win-x64 -p:Version=%version%.0.0 --self-contained true
 if not %ERRORLEVEL%==0 (
     echo "Build failed" && exit /b 1
 )
 
-set appDir=FileDB\bin\Release\net8.0-windows7.0\win-x64\publish
-set zipDir=FileDB-%version%
+set appDir=FileDBAvalonia.Desktop\bin\Release\net8.0\win-x64\publish
+set zipDir=FileDBAvalonia-%version%
 set releaseDir=release\%zipDir%
 set zipFilename=%zipDir%.zip
 
