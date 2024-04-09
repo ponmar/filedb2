bf2b0a2a5da61b1511f6c06fd4704a76a4b8a185
Pontus Markström <pontus.markstrom@gmail.com>
2024-04-09 20:03:53 +0200

    Update nuget packaages

--- update_licenses.bat
+++ update_licenses.bat
@@ -1,4 +1,3 @@
-dotnet-project-licenses -i FileDB.sln -u -o -j --output-directory FileDB\Resources\
 dotnet-project-licenses -i FileDB.sln -u -o -j --output-directory FileDBAvalonia\Resources\
 
 pause
