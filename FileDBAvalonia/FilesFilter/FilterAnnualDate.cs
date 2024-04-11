8d2f8f5190483ef64e6202523e0f60f5d86328a6
Pontus Markström <pontus.markstrom@gmail.com>
2024-04-11 22:19:33 +0200

    Validate new filter settings

--- FileDBAvalonia/FilesFilter/FilterAnnualDate.cs
+++ FileDBAvalonia/FilesFilter/FilterAnnualDate.cs
@@ -1,13 +1,12 @@
 ﻿using FileDBInterface.DatabaseAccess;
 using FileDBShared.Model;
-using System;
 using System.Collections.Generic;
 
 namespace FileDBAvalonia.FilesFilter;
 
 public class FilterAnnualDate(int startMonth, int startDay, int endMonth, int endDay) : IFilesFilter
 {
-    public bool CanRun() => true;
+    public bool CanRun() => startMonth < endMonth || (startMonth == endMonth && startDay <= endDay);
 
     public IEnumerable<FileModel> Run(IDatabaseAccess dbAccess)
     {
