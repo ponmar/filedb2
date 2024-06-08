27ace1e90163f4f9c82714f2247ff7cf4e9f130c
Pontus Markström <pontus.markstrom@gmail.com>
2024-06-08 15:35:33 +0200

    Fix license file path

--- update_licenses.bat
+++ update_licenses.bat
@@ -1,3 +1,3 @@
-dotnet-project-licenses -i FileDB.sln -u -o -j --output-directory FileDBAvalonia\Resources\
+dotnet-project-licenses -i FileDB.sln -u -o -j --output-directory FileDB\Resources\
 
 pause
