95dd81a99b1ad16c83b62d598bf55817d1082837
Pontus Markström <pontus.markstrom@gmail.com>
2024-09-28 11:53:01 +0200

    Add Avalonia port of MediaFilesHelper

---
+++ MediaFilesHelper2/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace MediaFilesHelper2.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MediaFilesHelper2/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace MediaFilesHelper2.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
